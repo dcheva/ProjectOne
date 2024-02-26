70fe54b828286937265df9710156ca33225d249c
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-02-26 15:55:19 +0200

    4 diods

--- project.a.0/project.a.0/project.a.0.ino
+++ project.a.0/project.a.0/project.a.0.ino
@@ -1,8 +1,36 @@
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(12, OUTPUT)
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int i;
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(9, HIGH);
+    delay(500);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(9, LOW);
+    delay(500);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    delay(500);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    delay(500);
+    digitalWrite(11, HIGH);
+    digitalWrite(9, HIGH);
+    delay(500);
+    digitalWrite(9, LOW);
+    delay(500);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    delay(500);
+    digitalWrite(9, LOW);
+    delay(500);
 }
